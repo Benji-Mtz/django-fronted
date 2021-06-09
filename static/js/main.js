bc3062f4e78c0cf5e9d567312c1bfaeecb6ca5b0
Benji Mtz <49083076+Benji-Mtz@users.noreply.github.com>
2021-06-09 00:35:26 -0500

    faltan media queries e iconos

--- static/js/main.js
+++ static/js/main.js
@@ -36,6 +36,27 @@ window.onclick = function(event) {
   }
 }
 
+var options = {
+  series: [44, 55, 41, 17, 15],
+  chart: {
+  type: 'donut',
+},
+responsive: [{
+  breakpoint: 480,
+  options: {
+    chart: {
+      width: 200
+    },
+    legend: {
+      position: 'bottom'
+    }
+  }
+}]
+};
+
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+chart.render();
+
 /* Highcharts.setOptions({
   colors: ['#01BAF2', '#71BF45', '#FAA74B']
 });  
