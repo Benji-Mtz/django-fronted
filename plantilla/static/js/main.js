c1e96516f8097df3b883f9c10432ad788e345bf9
Isaac Harari <hararimasri@gmail.com>
2021-06-10 11:53:33 -0500

    sidebar for mobile and desktop fixed

---
+++ plantilla/static/js/main.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+  $("#sidebar").mCustomScrollbar({
+    theme: "minimal",
+  });
+
+  $("#dismiss, .overlay").on("click", function () {
+    $("#sidebar").removeClass("active");
+    $(".overlay").removeClass("active");
+  });
+
+  $("#sidebarCollapse").on("click", function () {
+    $("#sidebar").toggleClass("active");
+    $("#content").toggleClass("active");
+    $(".overlay").addClass("active");
+    $(".collapse.in").toggleClass("in");
+    $("a[aria-expanded=true]").attr("aria-expanded", "false");
+  });
+});
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
+var options = {
+  series: [44, 55, 41, 17, 15],
+  chart: {
+    type: "donut",
+  },
+  responsive: [
+    {
+      breakpoint: 480,
+      options: {
+        chart: {
+          width: 200,
+        },
+        legend: {
+          position: "bottom",
+        },
+      },
+    },
+  ],
+};
+
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+chart.render();
