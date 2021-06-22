69f6746d0431d91a9dbc22817b9d20627ae95d3b
Benji Mtz <49083076+Benji-Mtz@users.noreply.github.com>
2021-06-22 02:26:55 -0500

    ajuste css y creación de login

--- plantilla/urls.py
+++ plantilla/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.login, name='login'),
     path('', views.index, name='index'),
     path('mantenimiento/', views.mantenimiento, name='mantenimiento'),
     path('admin/', admin.site.urls),
--- plantilla/views.py
+++ plantilla/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, redirect
 
+def login(request):
+    return render(request, 'login.html', {
+        'title': 'Login'
+    })
+
 def index(request):
     return render(request, 'index.html', {
         # context
